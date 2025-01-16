6cf669a91412bd5431730d6c4ab14f2cab4d92c2
tamilarasu-g <tamilaguru08150@gmail.com>
2025-01-16 15:42:33 +0530

    added dry run for ec2 and s3

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ ec2_client = boto3.client('ec2', region_name)
 s3_client = boto3.client('s3', region_name)
 s3_resource = boto3.resource('s3')
 
+# Delete EC2 Instances
+
 # Array to store the Instance IDs
 instanceIds = []
 
@@ -40,20 +42,12 @@ response = ec2_client.describe_instances(
     ]
 )
 
-# # Get the Instance Id from the response
+# Get the Instance Id from the response
 for i in response["Reservations"]:
     instance_id = i["Instances"][0]["InstanceId"]
     instanceIds.append(instance_id)
 
-print(f"Instance IDs are : {instanceIds} ")
-
-# Terminate the instances
-if len(instanceIds) > 0:
-    response = ec2_client.terminate_instances(
-        InstanceIds=instanceIds
-        )
-    print(response)
-
+# Delete Buckets
 
 # Get S3 buckets
 s3_client = boto3.client('s3', region_name='us-east-1')
@@ -64,29 +58,55 @@ buckets = []
 
 for response in response["Buckets"]:
     bucket = response["Name"]
-    bucket_tagging_response = s3_client.get_bucket_tagging(
+    try:
+        bucket_tagging_response = s3_client.get_bucket_tagging(
         Bucket=bucket
-    )
-
-    for tags in bucket_tagging_response['TagSet']:
-        if tags['Key'] == input_tag and tags['Value'] == input_value:
-            buckets.append(bucket)
-
-# print(f"Buckets are : {buckets}")
-
-if len(buckets) > 0:
-    for bucket in buckets:
-        bucket_versioning = s3_client.get_bucket_versioning(
-            Bucket=bucket
         )
-        print(f"For bucket : {bucket}")
-        status = bucket_versioning.get('Status')
-        obj_buck = s3_resource.Bucket(bucket)
-        if status == "Enabled":
-            obj_buck.object_versions.delete()
-            obj_buck.delete()
-            exit(0)
-        else:
-            obj_buck.objects.all().delete()
-            obj_buck.delete()
-            exit(0)
+        for tags in bucket_tagging_response['TagSet']:
+            if tags['Key'] == input_tag and tags['Value'] == input_value:
+                buckets.append(bucket)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NoSuchTagSet':
+            print(f"Bucket {bucket} has no tags")
+
+# Dry Run
+
+# Method to delete all resources
+def deleteresources():
+# Terminate the instances
+    if len(instanceIds) > 0:
+        response = ec2_client.terminate_instances(
+            InstanceIds=instanceIds
+            )
+        print(response)
+
+    # Delete Buckets
+    if len(buckets) > 0:
+        for bucket in buckets:
+            bucket_versioning = s3_client.get_bucket_versioning(
+                Bucket=bucket
+            )
+            # print(f"For bucket : {bucket}")
+            status = bucket_versioning.get('Status')
+            obj_buck = s3_resource.Bucket(bucket)
+            if status == "Enabled":
+                obj_buck.object_versions.delete()
+                obj_buck.delete()
+                exit(0)
+            else:
+                obj_buck.objects.all().delete()
+                obj_buck.delete()
+                exit(0)
+
+
+print(f"Following instances will be deleted : {instanceIds} ")
+print(f"Following buckets will be deleted : {buckets}")
+
+decision = input("Do you wish to continue ( y/N ) : ")
+
+if decision == "":
+    print("Nothing was deleted")
+    exit(0)
+elif decision == "y":
+    print("Deleting all the mentioned resources !!")
+    deleteresources()
